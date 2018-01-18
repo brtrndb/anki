e5e494d03da135f5720e70f7cc8abe70586c05c7
Bertrand B <boyer.bertrand@gmail.com>
2018-01-18 19:39:57 +0100

    Main class updated.

--- src/main/java/com/weekendesk/anki/Main.java
+++ src/main/java/com/weekendesk/anki/Main.java
@@ -1,4 +1,49 @@
 package com.weekendesk.anki;
 
-public final class Main {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.weekendesk.anki.core.AnkiGame;
+
+public final class Main
+{
+	private static final Logger log = LoggerFactory.getLogger(Main.class);
+
+	public static void main(final String[] args)
+	{
+		log.debug("Welcome.");
+		try
+		{
+			AnkiGame game = buildGame(args);
+			game.start();
+		}
+		catch (Exception e)
+		{
+			log.error("Oups... Something went wrong...", e);
+		}
+	}
+
+	/**
+	 * Build the game from command line parameters.
+	 * 
+	 * @param args
+	 * @return The game.
+	 */
+	private static AnkiGame buildGame(String[] args)
+	{
+		AnkiGame game;
+
+		if (args.length == 0)
+			game = new AnkiGame(null);
+		else
+		{
+			if (args.length > 1)
+				log.warn("There is more than one parameter. Only first will be used, others are ignored.");
+			game = new AnkiGame(args[0]);
+		}
+
+		log.debug("Game built.");
+
+		return (game);
+	}
 }
